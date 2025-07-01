40829d37ac520d6fde34b1f0f84acce2147cbb6b
soledadmonge <soledadmongea@gmail.com>
2025-07-01 18:26:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -189,4 +189,5 @@ register_callback("madrid", df_madrid, geo_madrid)
 register_callback("cataluna", df_cataluna, geo_cataluna)
 
 if __name__ == "__main__":
-    app.run_server(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8050)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8050)))
+
